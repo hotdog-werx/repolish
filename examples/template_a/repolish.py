9b9bc8a712b731e0a415dc7f3d04908b54ec4b77
Manuel Lopez <jmlopez.rod@gmail.com>
2025-10-17 07:58:38 -0500

    chore: rearrange example

--- examples/template_a/repolish.py
+++ examples/template_a/repolish.py
@@ -3,14 +3,15 @@ def create_context() -> dict[str, str]:
 
     This is intentionally small for the example.
     """
-    return {'package_name': 'myproj'}
+    return {'package_name': 'project'} # git can use used to figure out repo name
 
 
 def create_delete_files() -> list[str]:
     """Return a list of POSIX-style paths the provider proposes to delete."""
-    return ['myproj/old_file.txt']
+    return ['old_file.txt']
 
 
 def create_anchors() -> dict[str, str]:
     """Return example anchor replacements used by the example provider."""
     return {'extra-deps': '\nrequests = "^2.30"\n'}
+
