f693353550ef3a89ef19fa5ad8c9290ee4707488
Manuel Lopez <jmlopez.rod@gmail.com>
2026-07-22 20:22:55 -0500

    fix: build-publish.sh can be used manually
    Does not change anything in repolish, this is just maintenance.

--- .github/build-publish.sh
+++ .github/build-publish.sh
@@ -19,8 +19,7 @@ if [ -d dist ]; then
   rm -rf dist
 fi
 
-# no longer needed since we are using uvx hatch version in the post-changelog hook
-# uvx hatch version "${VERSION}"
+uvx hatch version "${VERSION}"
 uv build --no-sources
 
 export UV_PUBLISH_USERNAME=__token__
